e6bdce2ece0b60308ba321e21ec5555d752aae7f
= <=fengwukang@greaconsulting.com>
2021-07-21 15:38:20 +0800

    RSA秘钥生成，加密和解密

---
+++ common/src/main/java/dto/encrypt/EncryptPublicAndPrivateKey.java
@@ -0,0 +1,24 @@
+package dto.encrypt;
+
+public class EncryptPublicAndPrivateKey {
+    //公钥
+    private String publicKey;
+    //私钥
+    private String privateKey;
+
+    public String getPublicKey() {
+        return publicKey;
+    }
+
+    public void setPublicKey(String publicKey) {
+        this.publicKey = publicKey;
+    }
+
+    public String getPrivateKey() {
+        return privateKey;
+    }
+
+    public void setPrivateKey(String privateKey) {
+        this.privateKey = privateKey;
+    }
+}
---
+++ common/src/main/java/dto/encrypt/RSAEncrypt.java
@@ -0,0 +1,60 @@
+package dto.encrypt;
+
+
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import java.security.*;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+public class RSAEncrypt {
+    /*
+        用于生成一对RSA的秘钥（公钥，私钥）
+     */
+    public static EncryptPublicAndPrivateKey keyPair() throws NoSuchAlgorithmException {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        //设置秘钥的长度，1024位，基本不会被破解
+        keyPairGenerator.initialize(1024,new SecureRandom());
+
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+
+        String privateKeyStr = Base64.getEncoder().encodeToString(privateKey.getEncoded());
+        String publicKeyStr = Base64.getEncoder().encodeToString(publicKey.getEncoded());
+
+        EncryptPublicAndPrivateKey result = new EncryptPublicAndPrivateKey();
+        result.setPrivateKey(privateKeyStr);
+        result.setPublicKey(publicKeyStr);
+
+        return result;
+    }
+    /*
+        加密
+     */
+    public static byte[] encrypt(byte[] data,String publicKey) throws Exception{
+        byte[] decoded = Base64.getDecoder().decode(publicKey);
+        RSAPublicKey pubKey = (RSAPublicKey) KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(decoded));
+
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE,pubKey);
+        return cipher.doFinal(data);
+    }
+
+    /*
+        解密
+     */
+    public static byte[] decrypt(byte[] data,String privateKey) throws Exception{
+        byte[] decoded = Base64.getDecoder().decode(privateKey);
+        RSAPrivateKey priKey = (RSAPrivateKey) KeyFactory.getInstance("RSA").generatePrivate(new PKCS8EncodedKeySpec(decoded));
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE,priKey);
+        return cipher.doFinal(data);
+    }
+
+}
